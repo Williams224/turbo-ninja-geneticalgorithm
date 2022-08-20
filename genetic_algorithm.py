b5b1186c288df914edc621cad4537f6a5ed302ae
Tim Williams <timothy.williams@thetrainline.com>
2022-08-20 16:24:43 +0100

    drastially improved speed

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -6,73 +6,60 @@ from turbo_ninja_ga.fucks import uniform_fuck
 from turbo_ninja_ga.mutations import add_mutations
 
 
+def fast_random_choice(l):
+    return l[int(np.random.random() * len(l))]
+
+
 class FPLTeam:
 
-    full_data = []
-    gk_data = []
-    def_data = []
-    mid_data = []
-    fwd_data = []
+    full_data = {}
+    gk_data = {}
+    def_data = {}
+    mid_data = {}
+    fwd_data = {}
 
-    def __init__(self, data, from_chromosone=False):
-        if len(data) != 15:
+    def __init__(self, chromosone):
+        if len(chromosone) != 15:
             raise ValueError(" gene list must be of length 15, the size of an FPL team")
-        if from_chromosone:
-            if len(self.full_data) == 0:
-                raise RuntimeError(
-                    "Need to add reference data to class before initialising any instances from chromosone"
-                )
-
-            self.ref_data = self.full_data.loc[data]
-            self.chromosone = np.array(data)
-        else:
-            self.ref_data = data
-            self.chromosone = self.encode_chromosone()
-
+        if len(self.full_data) == 0:
+            raise RuntimeError(
+                "Need to add reference data to class before initialising any instances from chromosone"
+            )
+        self.chromosone = chromosone
         self.fitness_score = self.calc_fitness()
-
-        if len(self.gk_data) == 0:
-            self.gk_data = self.full_data[self.full_data.element_type == "GK"]
-        if len(self.def_data) == 0:
-            self.def_data = self.full_data[self.full_data.element_type == "DEF"]
-        if len(self.mid_data) == 0:
-            self.mid_data = self.full_data[self.full_data.element_type == "MID"]
-        if len(self.fwd_data) == 0:
-            self.fwd_data = self.full_data[self.full_data.element_type == "FWD"]
-
-        # print("Initialised")
-
-    def encode_chromosone(self):
-        return self.ref_data.sort_values(["element_type", "total_points"]).index.values
 
     def calc_fitness(self):
-        self.team_cost = np.sum(self.ref_data["now_cost"])
+        if len(set(self.chromosone)) != 15:
+            return 0.0
+        self.team_cost = np.sum(
+            np.array([self.full_data[id]["now_cost"] for id in self.chromosone])
+        )
         if self.team_cost > 1000:
             return 0.0
-        if self.ref_data.index.nunique() != 15:
-            return 0.0
-        return np.sum(self.ref_data["total_points"])
+        return np.sum(
+            np.array([self.full_data[id]["total_points"] for id in self.chromosone])
+        )
 
-    def mutate_position(self, x):
-        if x > len(self.chromosone) - 1:
-            raise ValueError(f"position {x} is out of range for mutation")
-        if x < 2:
-            self.chromosone[x] = np.random.choice(self.gk_data.index)
-        elif x < 7:
-            self.chromosone[x] = np.random.choice(self.def_data.index)
-        elif x < 11:
-            self.chromosone[x] = np.random.choice(self.mid_data.index)
+    def mutate_position(self, pos):
+        if pos > len(self.chromosone) - 1:
+            raise ValueError(" positon out of range ")
+        if pos < 2:
+            self.chromosone[pos] = fast_random_choice(list(self.gk_data.keys()))
+        elif pos < 7:
+            self.chromosone[pos] = fast_random_choice(list(self.def_data.keys()))
+        elif pos < 12:
+            self.chromosone[pos] = fast_random_choice(list(self.mid_data.keys()))
         else:
-            self.chromosone[x] = np.random.choice(self.fwd_data.index)
-
-        self.ref_data = self.full_data.loc[self.chromosone]
+            self.chromosone[pos] = fast_random_choice(list(self.fwd_data.keys()))
         self.fitness_score = self.calc_fitness()
 
 
-def generate_chromosone_df(gks, defs, mids, fwds):
-    return pd.concat(
-        [gks.sample(2), defs.sample(5), mids.sample(5), fwds.sample(3)], axis=0
-    )
+def generate_chromosone(gks, defs, mids, fwds):
+    gk_list = list(gks.sample(2).index)
+    defs_list = list(defs.sample(5).index)
+    mids_list = list(mids.sample(5).index)
+    fwds_list = list(fwds.sample(3).index)
+    return gk_list + defs_list + mids_list + fwds_list
 
 
 # def uniform_fuck(chromosone_A, chromosone_B):
@@ -132,9 +119,7 @@ class GeneticAlgorithm:
             ]
             breeding_pairs = np.random.choice(viable_parents, size=(n_children, 2))
             offspring = [
-                self.population_type(
-                    uniform_fuck(a.chromosone, b.chromosone), from_chromosone=True
-                )
+                self.population_type(uniform_fuck(a.chromosone, b.chromosone))
                 for a, b in breeding_pairs
             ]
             self.population = elite + offspring
@@ -152,26 +137,28 @@ class GeneticAlgorithm:
 if __name__ == "__main__":
 
     df = pd.read_csv("/Users/TimothyW/Fun/genetic_algorithm/fpl_player_data.csv")
+    df["player_id"] = df.index
 
     gks = df[df.element_type == "GK"]
     defs = df[df.element_type == "DEF"]
     mids = df[df.element_type == "MID"]
     fwds = df[df.element_type == "FWD"]
 
-    FPLTeam.full_data = df
+    FPLTeam.full_data = df.to_dict(orient="index")
+    FPLTeam.gk_data = gks.reindex(gks.player_id).to_dict(orient="index")
+    FPLTeam.def_data = defs.reindex(defs.player_id).to_dict(orient="index")
+    FPLTeam.mid_data = mids.reindex(mids.player_id).to_dict(orient="index")
+    FPLTeam.fwd_data = fwds.reindex(fwds.player_id).to_dict(orient="index")
 
     population = [
-        FPLTeam(
-            generate_chromosone_df(gks, defs, mids, fwds).index, from_chromosone=True
-        )
-        for _ in range(0, 100)
+        FPLTeam(generate_chromosone(gks, defs, mids, fwds)) for _ in range(0, 100)
     ]
 
     print("done")
 
     GA = GeneticAlgorithm(population)
 
-    GA.run()
+    GA.run(1000)
 
     # GA.get_best_solution
 
--- turbo_ninja_ga/mutations.py
+++ turbo_ninja_ga/mutations.py
@@ -6,5 +6,6 @@ def add_mutations(individual):
     mutation_posns = np.argwhere(
         np.random.uniform(size=len(individual.chromosone)) < mutation_probability
     )
+    mutation_posns = mutation_posns.reshape(mutation_posns.shape[0])
     for mp in mutation_posns:
         individual.mutate_position(mp)
